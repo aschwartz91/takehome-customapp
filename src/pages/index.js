0be31db8b78fa07155a8fd12a17c244f86310562
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2024-05-05 23:23:10 -0500

    adding in CSP config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import TaskList from '../components/TaskList';
-console.log("hello there")
 console.log(TaskList);
 
 export default function HomePage() {
   return (
     <div>
+      
       <TaskList />
     </div>
   );
