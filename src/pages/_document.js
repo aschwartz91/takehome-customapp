85396f8b8f4b071bf8f0d59fca878a4b40b44e32
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2024-05-06 00:22:19 -0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,11 +14,22 @@ class MyDocument extends Document {
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta http-equiv="Content-Security-Policy" content="
-        default-src 'self';
-        style-src 'self' 'unsafe-inline'; 
-        script-src 'self' 'unsafe-eval';  
-        connect-src 'self' https://api.airtable.com; 
-        img-src 'self';  
+          default-src 'self';
+          style-src 'self' 'unsafe-inline';
+          script-src-elem 'self' 'unsafe-eval' https://api.airtable.com 'unsafe-inline';
+          script-src 'self' 'unsafe-eval' https://api.airtable.com 'unsafe-inline' ;
+          
+          
+          img-src 'self' blob: data:;
+          connect-src 'self' https://api.airtable.com; 
+          font-src 'self';
+          object-src 'self';
+          base-uri 'self';
+          form-action 'self';
+          frame-ancestors 'none';
+          upgrade-insecure-requests;
+      
+      
     "/>
 
         </Head>
