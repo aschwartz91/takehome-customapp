bc592558b731148bb364839877a445975675b161
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2024-05-05 23:41:38 -0500

    more csp

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,24 @@
-// pages/_document.js
-
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html>
         <Head>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://apis.example.com; style-src 'self' 'https://fonts.googleapis.com'; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://example.com;"/>
+          <meta http-equiv="Content-Security-Policy" content="
+            default-src 'self';
+            style-src 'self' 'unsafe-inline';
+            script-src 'self' 'unsafe-eval' https://api.airtable.com;
+            connect-src 'self' https://api.airtable.com;
+          "/>
         </Head>
         <body>
           <Main />
